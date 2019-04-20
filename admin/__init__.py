da00c51d73b2efaef07f21b26d1ee7eae8481982
viccom <Pa88worddong>
2019-04-20 12:27:06 +0800

    flask add cross-domain

--- admin/__init__.py
+++ admin/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
+from flask_cors import *
 
 app = Flask(__name__)
-
+CORS(app, supports_credentials=True)
 
 def start_admin(vspc_manager, vspc_service):
     app.vspc_manager = vspc_manager
