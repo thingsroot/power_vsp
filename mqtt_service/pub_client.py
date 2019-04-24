8d864a743bf56755dfebf9759ecb5311b3310139
Dirk Chang <dirk@kooiot.com>
2019-04-24 14:10:07 +0800

    make an base clas for mqtt pub

---
+++ mqtt_service/pub_client.py
@@ -0,0 +1,70 @@
+import threading
+import logging
+import os
+import base64
+import paho.mqtt.client as mqtt
+from hbmqtt_broker.conf import MQTT_PROT
+
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    userdata.on_connect(client, flags, rc)
+
+
+def on_disconnect(client, userdata, rc):
+    userdata.on_disconnect(client, rc)
+
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    try:
+        userdata.on_message(client, msg)
+    except Exception as ex:
+        logging.exception(ex)
+
+
+class MQTTStreamPubBase(threading.Thread):
+    def __init__(self, service_name):
+        threading.Thread.__init__(self)
+        self.host = "localhost"
+        self.port = MQTT_PROT
+        self.clientid = "STREAM_PUB"
+        self.keepalive = 60
+        self.service_name = service_name
+
+    def stop(self):
+        self.mqttc.disconnect()
+
+    def run(self):
+        try:
+            mqttc = mqtt.Client(userdata=self, client_id=self.clientid)
+            self.mqttc = mqttc
+
+            mqttc.on_connect = on_connect
+            mqttc.on_disconnect = on_disconnect
+            mqttc.on_message = on_message
+
+            logging.debug('MQTT (%s) Connect to %s:%d cid: %s', self.service_name, self.host, self.port, self.clientid)
+            mqttc.connect_async(self.host, self.port, self.keepalive)
+
+            mqttc.loop_forever(retry_first_connection=True)
+        except Exception as ex:
+            logging.exception(ex)
+            os._exit(1)
+
+    def on_connect(self, client, flags, rc):
+        logging.info("MQTT (%s) %s connected return %d", self.service_name, self.host, rc)
+
+    def on_disconnect(self, client, rc):
+        logging.info("MQTT (%s) %s disconnect return %d", self.service_name, self.host, rc)
+
+    def on_message(self, client, msg):
+        logging.info("MQTT (%s) %s message recevied topic %s", self.service_name, self.host, msg.topic)
+
+    def publish(self, topic, payload, qos=1):
+        topic = "/v1/{0}/{1}".format(self.service_name, topic)
+        return self.mqttc.publish(topic=topic, payload=payload, qos=qos)
+
+    def subscribe(self, topic):
+        topic = "/v1/{0}/{1}".format(self.service_name, topic)
+        return self.mqttc.subscribe(topic)
--- vspc/mqtt_pub.py
+++ vspc/mqtt_pub.py
@@ -2,87 +2,33 @@ import threading
 import logging
 import os
 import base64
-import paho.mqtt.client as mqtt
-from hbmqtt_broker.conf import MQTT_PROT
+from mqtt_service.pub_client import MQTTStreamPubBase
 
 
-# The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, flags, rc):
-    userdata.on_connect(client, flags, rc)
-
-
-def on_disconnect(client, userdata, rc):
-    userdata.on_disconnect(client, rc)
-
-
-# The callback for when a PUBLISH message is received from the server.
-def on_message(client, userdata, msg):
-    try:
-        userdata.on_message(client, msg)
-    except Exception as ex:
-        logging.exception(ex)
-
-
-class MQTTStreamPub(threading.Thread):
+class MQTTStreamPub(MQTTStreamPubBase):
     def __init__(self):
-        threading.Thread.__init__(self)
-        self.host = "localhost"
-        self.port = MQTT_PROT
-        self.clientid = "STREAM_PUB"
-        self.keepalive = 60
-        self.sub_devs = set()
-
-    def stop(self):
-        self.mqttc.disconnect()
-
-    def run(self):
-        try:
-            mqttc = mqtt.Client(userdata=self, client_id=self.clientid)
-            self.mqttc = mqttc
-
-            mqttc.on_connect = on_connect
-            mqttc.on_disconnect = on_disconnect
-            mqttc.on_message = on_message
-
-            logging.debug('MQTT Connect to %s:%d cid: %s', self.host, self.port, self.clientid)
-            mqttc.connect_async(self.host, self.port, self.keepalive)
-
-            mqttc.loop_forever(retry_first_connection=True)
-        except Exception as ex:
-            logging.exception(ex)
-            os._exit(1)
-
-    def on_connect(self, client, flags, rc):
-        logging.info("MQTT %s connected return %d", self.host, rc)
-
-    def on_disconnect(self, client, rc):
-        logging.info("MQTT %s disconnect return %d", self.host, rc)
-
-    def on_message(self, client, msg):
-        logging.info("MQTT %s message recevied topic %s", self.host, msg.topic)
+        MQTTStreamPubBase.__init__(self, "vspc")
 
     def vspc_out_pub(self, key, data):
         topic = "VSPC_STREAM/{0}/OUT".format(key)
         payload = base64.b64encode(data)
-        return self.mqttc.publish(topic=topic, payload=payload, qos=1)
+        return self.publish(topic=topic, payload=payload, qos=1)
 
     def vspc_in_pub(self, key, data):
         topic = "VSPC_STREAM/{0}/IN".format(key)
         payload = base64.b64encode(data)
-        return self.mqttc.publish(topic=topic, payload=payload, qos=1)
+        return self.publish(topic=topic, payload=payload, qos=1)
 
     def socket_out_pub(self, key, data):
         topic = "SOCKET_STREAM/{0}/OUT".format(key)
         payload = base64.b64encode(data)
-        return self.mqttc.publish(topic=topic, payload=payload, qos=1)
+        return self.publish(topic=topic, payload=payload, qos=1)
 
     def socket_in_pub(self, key, data):
         topic = "SOCKET_STREAM/{0}/IN".format(key)
         payload = base64.b64encode(data)
-        return self.mqttc.publish(topic=topic, payload=payload, qos=1)
+        return self.publish(topic=topic, payload=payload, qos=1)
 
     def vspc_status(self, key, info):
         topic = "VSPC_STATUS/{0}".format(key)
-        return self.mqttc.publish(topic=topic, payload=info, qos=1)
-
-
+        return self.publish(topic=topic, payload=info, qos=1)
